720804bc094f763dbd122686d6e5e4d826e0c13c
mbordon22 <rivadenneira@gmail.com>
2021-06-12 00:21:00 -0300

    cambios inicio y productos, prueba con json productos

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const expHbs = require("express-handlebars");
+const productos = require('./productos.json');
 
 const path = require('path');
 const PUERTO = 3000;
@@ -20,11 +21,29 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static(path.join(__dirname, "client")));
 
+
+/* ------------------------------------------------Pantalla de INICIO---------------------------------------- */
 app.get("/", (req, res) => {
+
+    let resultado = productos;
+
+    resultado = resultado.filter(producto => producto.categoria === "pc_escritorio");
+
     res.render("inicio", {
-        titulo : "Inicio"
+        titulo : "Inicio",
+        productos : resultado.slice(0,3)
     });
 })
+
+/* ------------------------------------------------Pantalla de PRODUCTOS---------------------------------------- */
+app.get("/productos", (req, res) => {
+    res.render("productos", {
+        titulo : "Productos",
+        productos
+    })
+})
+
+
 
 app.listen(PUERTO, () => {
     console.log("Escuchando puerto "+ PUERTO);
